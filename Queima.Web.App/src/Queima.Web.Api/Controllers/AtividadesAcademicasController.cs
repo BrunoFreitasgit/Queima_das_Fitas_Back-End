71f432702d27c23d448fcc7701540c283f3d26af
1130504 <1130504@isep.ipp.pt>
2016-08-15 18:43:50 +0100

    Alteração WebApi Controller das AtividadesAcademicas

--- Queima.Web.App/src/Queima.Web.Api/Controllers/AtividadesAcademicasController.cs
+++ Queima.Web.App/src/Queima.Web.Api/Controllers/AtividadesAcademicasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Queima.Web.Api.DTOs;
 using Queima.Web.App.Interfaces;
 using Queima.Web.App.Models;
 using Queima.Web.App.ViewModels;
@@ -24,19 +25,19 @@ namespace Queima.Web.Api.Controllers
 
         // GET: api/AtividadesAcademicas
         [HttpGet]
-        public async Task<IEnumerable<AtividadeAcademicaViewModel>> GetConcursos()
+        public async Task<IEnumerable<AtividadeAcademicaDTO>> GetAtividadesAcademicas()
         {
             IEnumerable<AtividadeAcademica> lista = await _repository.FindAll();
             IEnumerable<LocalAtividadeAcademica> lista_locais = await _locaisRepository.FindAll();
-            var lista_vm = new List<AtividadeAcademicaViewModel>();
+            var lista_dto = new List<AtividadeAcademicaDTO>();
 
             foreach (var atividade in lista)
             {
                 atividade.LocalAtividadeAcademica = lista_locais.Single(l => l.Id == atividade.LocalAtividadeAcademicaId);
-                var vm = new AtividadeAcademicaViewModel(atividade);
-                lista_vm.Add(vm);
+                var vm = new AtividadeAcademicaDTO(atividade);
+                lista_dto.Add(vm);
             }
-            return lista_vm;
+            return lista_dto;
         }
 
     }
---
+++ Queima.Web.App/src/Queima.Web.Api/DTOs/AtividadeAcademicaDTO.cs
@@ -0,0 +1,36 @@
+﻿using Queima.Web.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Queima.Web.Api.DTOs
+{
+    public class AtividadeAcademicaDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string Preco { get; set; } = "0.00";
+        public string Data { get; set; } = string.Empty;
+        public string Local { get; set; }
+        public double LocalLatitude { get; set; }
+        public double LocalLongitude { get; set; }
+        public string ImagemUrl { get; set; } = string.Empty;
+        public string PontosVenda { get; set; } = string.Empty;
+
+        public AtividadeAcademicaDTO(AtividadeAcademica a)
+        {
+            Id = a.Id;
+            Nome = a.Nome;
+            Descricao = a.Descricao;
+            Preco = a.Preco.ToString();
+            Data = string.Concat(a.Data.Day + "/" + a.Data.Month + "/" + a.Data.Year);
+            Local = a.LocalAtividadeAcademica.Nome;
+            LocalLatitude = a.LocalAtividadeAcademica.Latitude;
+            LocalLongitude = a.LocalAtividadeAcademica.Longitude;
+            ImagemUrl = a.ImagemUrl;
+            PontosVenda = a.PontosVenda;
+        }
+    }
+}
